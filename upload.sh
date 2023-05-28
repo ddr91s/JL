ac84cd215b015bab4004dd4e7d1ba78e548b6752
ddr91s <115823154+ddr91s@users.noreply.github.com>
2023-05-28 20:13:54 +0700

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -9,9 +9,7 @@ fi
 FILE="@$1"
 
 # Find the Best server to upload
-SERVER=$(curl -s https://apiv2.gofile.io/getServer | jq  -r '.data|.server')
-
-UPLOAD=$(curl -F file=${FILE} https://${SERVER}.gofile.io/uploadFile --max-time 1800 --retry 1 --retry-delay 0 --progress-bar)
+UPLOAD=$(curl -F file=${FILE} https://store5.gofile.io/uploadFile --max-time 1800 --retry 1 --retry-delay 0 --progress-bar)
 
 LINK=$(echo $UPLOAD | jq -r '.data|.downloadPage')
 fileName=$(echo $UPLOAD | jq -r '.data|.fileName')
